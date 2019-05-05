b00e2a2e83269420f1b76f1f5f958ae6087975ff
Daniel Flower <git@danielflower.com>
2019-05-05 18:24:59 +0800

    Added a handler which displays the request headers sent to the server

--- src/main/java/trace/App.java
+++ src/main/java/trace/App.java
@@ -3,14 +3,17 @@ package trace;
 import io.muserver.ContentTypes;
 import io.muserver.Method;
 import io.muserver.MuServer;
+import j2html.tags.ContainerTag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static io.muserver.ContextHandlerBuilder.context;
 import static io.muserver.MuServerBuilder.muServer;
 import static io.muserver.handlers.ResourceHandlerBuilder.fileOrClasspath;
+import static j2html.TagCreator.*;
 
 public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
@@ -28,9 +31,37 @@ public class App {
             .withHttpPort(port)
             .addHandler(
                 context(appName)
-                    .addHandler(Method.GET, "/hello/{name}", (req, resp, params) -> {
+                    .addHandler(null, "/", (req, resp, params) -> {
                         resp.contentType(ContentTypes.TEXT_HTML);
-                        resp.write("Hello, <b>" + params.get("name") + "</b>!");
+
+                        html(
+                            head(
+                                title("Trace - see your request info"),
+                                link().withRel("stylesheet").withHref("/trace/styles.css")
+                            ),
+                            body(
+                                j2html.TagCreator.main(
+                                    h1("Trace"),
+                                    p("Use this to see the request info sent to the server."),
+                                    p( text("Recieved "), strong(req.method().name()), text(" "), code(req.uri().getPath()), text("with querystring: "), code(req.uri().getQuery())),
+                                    table(
+                                        thead(tr(th("Request header"), th("Value"))),
+                                        tbody(
+                                            Stream.generate(req.headers().iterator()::next)
+                                                .limit(req.headers().size())
+                                                .map(entry ->
+                                                    tr(
+                                                        td(attrs(".headerName"), entry.getKey()),
+                                                        td(attrs(".headerValue"), entry.getValue())
+                                                    ))
+                                                .toArray(ContainerTag[]::new)
+                                        )
+                                    )
+                                )
+                            )
+                        ).render(resp.writer());
+
+
                     })
                     .addHandler(fileOrClasspath("src/main/resources/web", "/web"))
             )
